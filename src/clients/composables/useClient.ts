2e7558fad385991b3940fc37cf706f27cf4bdbd8
maxijayme <maxijayme@gmail.com>
2025-02-23 10:27:54 +0100

    create useClient composable

---
+++ src/clients/composables/useClient.ts
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/vue-query";
+import { ref, watch } from "vue";
+import type { Client } from "@/clients/interfaces/client";
+import clientsApi from "@/api/clients-api";
+
+const getClient = async( id: number ) => {
+  const {data} = await clientsApi.get<Client>(`/clients/${id}`);
+  return data;
+}
+
+const useClient = (id:number) => {
+
+  const client = ref<Client>();
+
+  const {data, isLoading} = useQuery({
+    queryKey: ['client', 'id', id],
+    queryFn:() => getClient(id),
+  })
+
+  watch(data, () => {
+    if (data.value) {
+      client.value = data.value;
+    }
+  }, {immediate: true})
+
+   return {
+    client,
+    isLoading
+   }
+}
+
+export default useClient;
--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -1,13 +1,52 @@
 <script setup lang="ts">
+import LoadingModal from '@/shared/components/LoadingModal.vue';
+import { useRoute } from 'vue-router';
+import useClient from '../composables/useClient';
+
+const id = useRoute().params.id;
+
+const {client, isLoading} = useClient(+id);
 
 </script>
 
 <template>
+  <h3>Guardando...</h3>
+  <h3>Guardo</h3>
+  <LoadingModal v-if=isLoading />
   <div>
-    <h1>Cliente por ID</h1>
+    <h1>PEPE</h1>
+    <form></form>
+      <input type="text" placeholder="Nombre del cliente"/>
+      <input type="text" placeholder="Dirección"/>
+      <button type="submit">Guardar</button>
   </div>
+  <code>
+    {{ client }}
+  </code>
 </template>
 
 <style scoped>
+  input {
+    width: 100%;
+    padding: 5px 10px;
+    margin-bottom: 10px;
+  }
+  button {
+    background-color: transparent;
+    border-radius: 5px;
+    border: 1px solid var( --color-border );
+    color: var( --color-text );
+    cursor: pointer;
+    margin-right: 5px;
+    padding: 10px;
+    transition: all .5s;
+}
 
+button:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+    transition: all .5s;
+}
+button:disabled {
+    cursor: not-allowed;
+}
 </style>
